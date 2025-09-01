a9a12e41dbf7c44420664d37c56cd25b9ab58b97
tolstenkaya <o.tutova.82@gmail.com>
2025-09-01 21:11:10 +0300

    add isOperationSymbol method and change hierarchy

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,46 @@
+import java.util.Date;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        String str = scan.nextLine();
+        String str;
 
         do{
+            do{
+                System.out.print("Enter number: ");
+                str = scan.nextLine().trim();
+            }while(!isNumber(str));
 
-        }while(!isNumber(str));
+            double num1 = Double.parseDouble(str);
+
+            do{
+                System.out.print("Enter operation: ");
+                str=scan.nextLine().trim();
+            }while (!isOperationSymbol(str));
+
+            char operation = str.charAt(0);
+
+            do{
+                System.out.print("Enter operation: ");
+                str=scan.nextLine().trim();
+            }while (!isNumber(str));
+
+            double num2 = Double.parseDouble(str);
+
+        }while(!str.isEmpty());
     }
 
     public static boolean isNumber(String str){
         if(str.chars().allMatch(c->Character.isDigit(c))){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public static boolean isOperationSymbol(String str){
+        if(str.chars().allMatch(c->c=='+' || c=='-' || c=='*' || c=='/')){
             return true;
         }else{
             return false;
