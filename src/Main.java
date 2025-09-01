2b164ece424e5434db1245879dfaaecb64dfbc8a
tolstenkaya <o.tutova.82@gmail.com>
2025-09-01 20:42:26 +0300

    add isNumber method

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,18 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        String str = scan.nextLine();
+
         do{
 
-        }while()
+        }while(!isNumber(str));
+    }
+
+    public static boolean isNumber(String str){
+        if(str.chars().allMatch(c->Character.isDigit(c))){
+            return true;
+        }else{
+            return false;
+        }
     }
 }
